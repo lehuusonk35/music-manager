2916751b72161926c14cab212bacb65d2c4b5762
lehuusonk35 <lehuusonk35@gmail.com>
2017-11-21 00:46:39 +0700

    sua o nha

--- app/scripts/controllers/SongCtrl.js
+++ app/scripts/controllers/SongCtrl.js
@@ -12,48 +12,65 @@ angular.module('myngappAppApp').controller('SongCtrl', function ($scope, SongSer
   $scope.bread = 'Song';
   $scope.songs = SongService.list();
   $scope.selectedList = [];
+  $scope.isSelectedAll = false;
   $scope.saveSong = function () {
     SongService.save($scope.newsong);
     $scope.newsong = {};
+    $scope.isSelectedAll = allChecked();
   }
 
 
   $scope.delete = function (id) {
-
     SongService.delete(id);
-    if ($scope.newsong.id == id) $scope.newsong = {};
   }
-
 
   $scope.edit = function (id) {
     $scope.newsong = angular.copy(SongService.get(id));
   }
-  $scope.CheckUncheckHeader = function () {
-    $scope.IsAllChecked = true;
-    for (var i = 0; i < $scope.songs.length; i++) {
-      if (!$scope.songs[i].Selected) {
-        $scope.IsAllChecked = false;
-        break;
-      }
-    };
-  };
-  $scope.CheckUncheckHeader();
 
-  $scope.CheckUncheckAll = function () {
-    for (var i = 0; i < $scope.songs.length; i++) {
-      $scope.songs[i].Selected = $scope.IsAllChecked;
+  $scope.selectAll = function(){
+    $scope.isSelectedAll= !$scope.isSelectedAll;
+    $scope.selectedList = [];
+    for(var i =0;i<$scope.songs.length;i++){
+      $scope.songs[i].isSelected = $scope.isSelectedAll;
+      if($scope.songs[i].isSelected){
+        $scope.selectedList.push($scope.songs[i]);
+      }
     }
-  };
+  }
+
+  $scope.selectSong = function(song){
+    song.isSelected=!song.isSelected;
+    if(song.isSelected){
+      $scope.selectedList.push(song);
+    }
+    else {
+      for(var i =0;i<$scope.selectedList.length;i++){
+        if($scope.selectedList[i].id === song.id){
+          $scope.selectedList.splice(i,1);
+        }
+      }
+    }
+    $scope.isSelectedAll = allChecked();
+  }
+
+  function allChecked(){
+    var result = true;
+    for(var i =0;i<$scope.songs.length;i++){
+      if(!$scope.songs[i].isSelected){
+        result = false;
+      }
+    }
+
+    return result;
+  }
+
   $scope.removeSelectedRows = function() {
     //start from last index because starting from first index cause shifting
     //in the array because of array.splice()
-    for ( var i = 0; i < $scope.songs.length; i++) {
-      if ($scope.m.Selected[i]) {
-        //delete row from data
-        $scope.songs.splice(i, 1);
-        //delete rowSelection property
-        delete $scope.m.Selected[i];
-      }
+    for ( var i = 0; i < $scope.selectedList.length; i++) {
+      var id = $scope.selectedList[i].id;
+      SongService.delete(id);
     }
   };
 });
